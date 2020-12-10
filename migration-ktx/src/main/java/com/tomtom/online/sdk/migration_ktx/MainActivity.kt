f690b86a1f396a096a5c68a06d241279af616847
Mateusz Szczepanczyk <mateusz.szczepanczyk@tomtom.com>
2020-12-10 11:16:01 +0100

    Version update and small improvements

--- migration-ktx/src/main/java/com/tomtom/online/sdk/migration_ktx/MainActivity.kt
+++ migration-ktx/src/main/java/com/tomtom/online/sdk/migration_ktx/MainActivity.kt
@@ -2,18 +2,13 @@ package com.tomtom.online.sdk.migration_ktx
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.tomtom.online.sdk.common.location.LatLng
 import com.tomtom.online.sdk.map.*
 import com.tomtom.online.sdk.routing.OnlineRoutingApi
-import com.tomtom.online.sdk.routing.data.RouteQueryBuilder
-import com.tomtom.online.sdk.routing.data.RouteResponse
-import com.tomtom.online.sdk.routing.data.RouteType
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.observers.DisposableSingleObserver
-import io.reactivex.schedulers.Schedulers
+import com.tomtom.online.sdk.routing.RoutingException
+import com.tomtom.online.sdk.routing.route.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -23,40 +18,50 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         (mapFragment as MapFragment).getAsyncMap(this)
-        btnTrafficOn.setOnClickListener {
-            map.trafficSettings.turnOnVectorTrafficIncidents()
-            map.trafficSettings.turnOnVectorTrafficFlowTiles()
-        }
-        btnTrafficOff.setOnClickListener { map.trafficSettings.turnOffTraffic() }
-        btnRouteShow.setOnClickListener {
-            val amsterdam = LatLng(52.37, 4.90)
-            val hague = LatLng(52.07, 4.30)
-            val routingApi = OnlineRoutingApi.create(applicationContext)
-            val routeQuery = RouteQueryBuilder(amsterdam, hague).withRouteType(RouteType.FASTEST).build()
-            routingApi.planRoute(routeQuery)
-                    .subscribeOn(Schedulers.newThread())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(object : DisposableSingleObserver<RouteResponse?>() {
-                        override fun onSuccess(routeResponse: RouteResponse) {
-                            for (fullRoute in routeResponse.routes) {
-                                val routeBuilder = RouteBuilder(
-                                        fullRoute.coordinates)
-                                map.addRoute(routeBuilder)
-                            }
-                        }
-
-                        override fun onError(e: Throwable) {
-                            Toast.makeText(this@MainActivity, e.localizedMessage, Toast.LENGTH_LONG).show()
-                        }
-                    })
-        }
     }
 
     override fun onMapReady(tomtomMap: TomtomMap) {
         map = tomtomMap
+        btnTrafficOn.setOnClickListener {
+            map.trafficSettings.turnOnTrafficIncidents()
+            map.trafficSettings.turnOnTrafficFlowTiles()
+        }
+        btnTrafficOff.setOnClickListener {
+            map.trafficSettings.turnOffTrafficIncidents()
+            map.trafficSettings.turnOffTrafficFlowTiles()
+        }
+        btnRouteShow.setOnClickListener(showRouteListener)
+
         val amsterdam = LatLng(52.37, 4.90)
         val balloon = SimpleMarkerBalloon("Amsterdam")
         tomtomMap.addMarker(MarkerBuilder(amsterdam).markerBalloon(balloon))
         tomtomMap.centerOn(CameraPosition.builder().focusPosition(amsterdam).zoom(7.0).build())
+    }
+
+    private val showRouteListener = View.OnClickListener {
+        val amsterdam = LatLng(52.37, 4.90)
+        val hague = LatLng(52.07, 4.30)
+        val routingApi = OnlineRoutingApi.create(applicationContext, BuildConfig.ROUTING_API_KEY)
+        val routeDescriptor = RouteDescriptor.Builder()
+                .routeType(com.tomtom.online.sdk.routing.route.description.RouteType.FASTEST)
+                .build()
+        val routeCalculationDescriptor = RouteCalculationDescriptor.Builder()
+                .routeDescription(routeDescriptor).build()
+        val routeSpecification = RouteSpecification.Builder(amsterdam, hague)
+                .routeCalculationDescriptor(routeCalculationDescriptor)
+                .build()
+        routingApi.planRoute(routeSpecification, object : RouteCallback {
+            override fun onSuccess(routePlan: RoutePlan) {
+                for (fullRoute in routePlan.routes) {
+                    val routeBuilder = RouteBuilder(
+                            fullRoute.getCoordinates())
+                    map.addRoute(routeBuilder)
+                }
+            }
+
+            override fun onError(error: RoutingException) {
+                Toast.makeText(this@MainActivity, error.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+        })
     }
 }
--- migration/src/main/java/com/tomtom/online/sdk/migration/MainActivity.java
+++ migration/src/main/java/com/tomtom/online/sdk/migration/MainActivity.java
@@ -1,6 +1,7 @@
 package com.tomtom.online.sdk.migration;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -17,70 +18,91 @@ import com.tomtom.online.sdk.map.SimpleMarkerBalloon;
 import com.tomtom.online.sdk.map.TomtomMap;
 import com.tomtom.online.sdk.routing.OnlineRoutingApi;
 import com.tomtom.online.sdk.routing.RoutingApi;
-import com.tomtom.online.sdk.routing.data.FullRoute;
-import com.tomtom.online.sdk.routing.data.RouteQuery;
-import com.tomtom.online.sdk.routing.data.RouteQueryBuilder;
-import com.tomtom.online.sdk.routing.data.RouteResponse;
-import com.tomtom.online.sdk.routing.data.RouteType;
+import com.tomtom.online.sdk.routing.RoutingException;
+import com.tomtom.online.sdk.routing.route.RouteCalculationDescriptor;
+import com.tomtom.online.sdk.routing.route.RouteCallback;
+import com.tomtom.online.sdk.routing.route.RouteDescriptor;
+import com.tomtom.online.sdk.routing.route.RoutePlan;
+import com.tomtom.online.sdk.routing.route.RouteSpecification;
+import com.tomtom.online.sdk.routing.route.information.FullRoute;
 
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.observers.DisposableSingleObserver;
-import io.reactivex.schedulers.Schedulers;
+import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private TomtomMap map;
+    private Button btnTrafficOn;
+    private Button btnTrafficOff;
+    private Button btnRouteShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MapFragment mapFragment = (MapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.mapFragment);
-        mapFragment.getAsyncMap(this);
+        MapFragment mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.mapFragment);
+        if(mapFragment != null) {
+            mapFragment.getAsyncMap(this);
+        }
 
-        Button btnTrafficOn = findViewById(R.id.btnTrafficOn);
-        btnTrafficOn.setOnClickListener(v -> {
-            map.getTrafficSettings().turnOnVectorTrafficIncidents();
-            map.getTrafficSettings().turnOnVectorTrafficFlowTiles();
-        });
-
-        Button btnTrafficOff = findViewById(R.id.btnTrafficOff);
-        btnTrafficOff.setOnClickListener(v -> map.getTrafficSettings().turnOffTraffic());
-
-        Button btnRouteShow = findViewById(R.id.btnRouteShow);
-        btnRouteShow.setOnClickListener(v -> {
-            LatLng amsterdam = new LatLng(52.37, 4.90);
-            LatLng hague = new LatLng(52.07, 4.30);
-            RoutingApi routingApi = OnlineRoutingApi.create(getApplicationContext());
-            RouteQuery routeQuery = new RouteQueryBuilder(amsterdam, hague).withRouteType(RouteType.FASTEST).build();
-            routingApi.planRoute(routeQuery)
-                    .subscribeOn(Schedulers.newThread())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(new DisposableSingleObserver<RouteResponse>() {
-                        @Override
-                        public void onSuccess(RouteResponse routeResponse) {
-                            for (FullRoute fullRoute : routeResponse.getRoutes()) {
-                                RouteBuilder routeBuilder = new RouteBuilder(
-                                        fullRoute.getCoordinates());
-                                map.addRoute(routeBuilder);
-                            }
-                        }
-                        @Override
-                        public void onError(Throwable e) {
-                            Toast.makeText(MainActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
-                        }
-                    });
-        });
+        btnTrafficOn = findViewById(R.id.btnTrafficOn);
+        btnTrafficOff = findViewById(R.id.btnTrafficOff);
+        btnRouteShow = findViewById(R.id.btnRouteShow);
     }
 
     @Override
     public void onMapReady(@NonNull TomtomMap tomtomMap) {
         this.map = tomtomMap;
 
+        btnTrafficOn.setOnClickListener(v -> {
+            map.getTrafficSettings().turnOnTrafficIncidents();
+            map.getTrafficSettings().turnOnTrafficFlowTiles();
+        });
+
+        btnTrafficOff.setOnClickListener(v -> {
+            map.getTrafficSettings().turnOffTrafficFlowTiles();
+            map.getTrafficSettings().turnOffTrafficIncidents();
+        });
+
+        btnRouteShow.setOnClickListener(showRouteListener);
+
         LatLng amsterdam = new LatLng(52.37, 4.90);
         SimpleMarkerBalloon balloon = new SimpleMarkerBalloon("Amsterdam");
         tomtomMap.addMarker(new MarkerBuilder(amsterdam).markerBalloon(balloon));
         tomtomMap.centerOn(CameraPosition.builder().focusPosition(amsterdam).zoom(7.0).build());
     }
+
+    private View.OnClickListener showRouteListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            LatLng amsterdam = new LatLng(52.37, 4.90);
+            LatLng hague = new LatLng(52.07, 4.30);
+            RoutingApi routingApi = OnlineRoutingApi.create(getApplicationContext(), BuildConfig.ROUTING_API_KEY);
+
+            RouteDescriptor routeDescriptor = new RouteDescriptor.Builder()
+                    .routeType(com.tomtom.online.sdk.routing.route.description.RouteType.FASTEST)
+                    .build();
+            RouteCalculationDescriptor routeCalculationDescriptor = new RouteCalculationDescriptor.Builder()
+                    .routeDescription(routeDescriptor)
+                    .build();
+            RouteSpecification routeSpecification = new RouteSpecification.Builder(amsterdam, hague)
+                    .routeCalculationDescriptor(routeCalculationDescriptor)
+                    .build();
+
+            routingApi.planRoute(routeSpecification, new RouteCallback() {
+                @Override
+                public void onSuccess(@NotNull RoutePlan routePlan) {
+                    for (FullRoute fullRoute : routePlan.getRoutes()) {
+                        RouteBuilder routeBuilder = new RouteBuilder(
+                                fullRoute.getCoordinates());
+                        map.addRoute(routeBuilder);
+                    }
+                }
+
+                @Override
+                public void onError(@NotNull RoutingException e) {
+                    Toast.makeText(MainActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    };
 }
